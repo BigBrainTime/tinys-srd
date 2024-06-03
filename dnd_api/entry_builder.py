d5ba75ccac4b65566661ee82ae7b1b96fa434fbc
BigBrainTime <57951435+BigBrainTime@users.noreply.github.com>
2024-06-03 17:26:52 -0500

    Update entry_builder.py

--- dnd_api/entry_builder.py
+++ dnd_api/entry_builder.py
@@ -1,6 +1,6 @@
 class entry_builder():
     def __repr__(self) -> str:
-        return self.__dict__
+        return str(self.__dict__)
     
     def __init__(self, **kwargs) -> None:
         for kwarg in kwargs:
